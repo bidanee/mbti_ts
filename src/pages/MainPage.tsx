c4cb47002e743e30cd1b722966cdad9783bcd763
bidanee <dleksql119@gmail.com>
2023-07-12 13:39:31 +0900

    main view

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -20,12 +20,12 @@ export default function MainPage() {
               <img src={MainImg} />
             </div>
           </div>
-          <div className="flex flex-col items-center justify-center mb-5">
+          <div className="flex flex-col items-center justify-center mb-5 ">
             <div>나랑 잘 맞는 고양이 찾기!</div>
             <div>내가 집사가 되서 고양이를 키운다면..?</div>
           </div>
           <button
-            className="btn btn-error text-2xl mb-5 "
+            className="btn btn-error text-2xl mb-5 text-white"
             onClick={handleClickButton}
           >
             테스트 시작하기
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-color": "#f0f8f6",
+        "medium-color": "#d6f7f3",
+        "base-color": "#f4f4f4",
+      },
+      fontFamily: {
+        sans: ["GmarketSansMedium"],
+      },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1025px",
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
+  daisyui: {
+    styled: true,
+    themes: ["light"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
 };
