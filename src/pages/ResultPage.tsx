c009827eaa5319dd8ffe6bcfab3bbbe0a6ea543b
bidanee <dleksql119@gmail.com>
2023-07-14 16:18:55 +0900

    사이트명 변경

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -23,18 +23,28 @@ export default function ResultPage() {
           </div>
 
           <div className="flex flex-col items-center justify-center mb-5 ">
-            <div className=" w-full mb-2 px-5 text-lg text-center">
+            <div className=" w-full mb-1 px-5 text-lg text-center">
               {testResult?.best}형 예비집사님과 찰떡궁합인 고양이는
               {testResult?.mbti}형 고양이 {testResult?.name}입니다.
             </div>
-            <div className="px-5 text-base font-normal">{testResult?.desc}</div>
-            <div className="px-8 mt-2 text-center text-blue-600">
+            <div className="px-5 text-sm mt-1 mb-1 font-normal">
+              {testResult?.desc}
+            </div>
+            <div className="px-8 mt-1 text-center text-blue-600 ">
               나의 고양이와 잘 맞는 형제묘로는 {friendCat?.name} 추천드려요.
             </div>
           </div>
+          <div className="flex">
+            <button className="btn btn-error mr-3 text-white">
+              테스트 다시하기
+            </button>
+            <button className="btn btn-warning text-white">
+              카카오톡 공유하기
+            </button>
+          </div>
         </div>
-        <div className="adfit" />
       </div>
+      <div className="adfit" />
     </>
   );
 }
